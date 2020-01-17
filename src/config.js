755700fdf382513573d2c03f4d22567665d244e9
Abhishek Wadhwa <awadhwa@architech.ca>
2020-01-17 15:34:33 -0500

    Setting up out React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-serverless-stack-practice"
+},
+apiGateway: {
+REGION: "us-east-1",
+URL: "https://aakohv7m7i.execute-api.us-east-1.amazonaws.com/prod"
+},
+cognito: {
+REGION: "us-east-1",
+USER_POOL_ID: "us-east-1_HQSCCHVUq",
+APP_CLIENT_ID: "3bmpctrrp8gg11guv2c6qhhe5g",
+IDENTITY_POOL_ID: "us-east-1:feaf1acc-49d9-46eb-b37b-522f39945e40"
+}
+};
